9d55513934f5e9f19109303ce59a4ee078ea33cd
Hamid Khodabandehloo <hamid@dropbox.com>
2022-11-07 20:05:30 -0800

    fix py code style

--- backend/models.py
+++ backend/models.py
@@ -12,6 +12,8 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 """
+
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -19,16 +21,18 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
+
 """
 Question
 
 """
+
+
 class Question(db.Model):
     __tablename__ = 'questions'
-
     id = Column(Integer, primary_key=True)
-    question = Column(String, nullable = False)
-    answer = Column(String, nullable = False)
+    question = Column(String, nullable=False)
+    answer = Column(String, nullable=False)
     category = Column(Integer)
     difficulty = Column(Integer)
 
@@ -48,16 +52,20 @@ class Question(db.Model):
     def delete(self):
         db.session.delete(self)
         db.session.commit()
-    
+
     def search_by_question(search_phrase):
-        return [q.format() for q in Question.query.filter(func.lower(Question.question).contains(search_phrase.lower()))]
+        return [q.format() for q in Question.query.filter(
+            func.lower(Question.question).contains(search_phrase.lower()))]
 
     def get_new_question(old_question_ids, category):
         if category == 0:
-            return Question.query.filter(~Question.id.in_(old_question_ids)).first().format()
+            return Question.query.filter(
+                ~Question.id.in_(old_question_ids)).first().format()
         else:
-            return Question.query.filter(and_(~Question.id.in_(old_question_ids), Question.category == category)).first().format()
-        
+            return Question.query.filter(
+                and_(~Question.id.in_(old_question_ids),
+                     Question.category == category)).first().format()
+
     def format(self):
         return {
             'id': self.id,
@@ -67,10 +75,13 @@ class Question(db.Model):
             'difficulty': self.difficulty
             }
 
+
 """
 Category
 
 """
+
+
 class Category(db.Model):
     __tablename__ = 'categories'
 
@@ -83,7 +94,7 @@ class Category(db.Model):
     def insert(self):
         db.session.add(self)
         db.session.commit()
-        
+
     def format(self):
         return {
             'id': self.id,
