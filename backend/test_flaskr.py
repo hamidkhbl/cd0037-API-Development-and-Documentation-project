a39056b5a85301dd90d31fb1a47de4fdd4051611
Hamid Khodabandehloo <hamid@dropbox.com>
2022-11-07 22:12:45 -0800

    Add more test cases for failure cases

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -31,7 +31,6 @@ class TriviaTestCase(unittest.TestCase):
         pass
 
     # Test POST Category
-
     def test_post_category(self):
         category = Category(type='test')
         payload = category.format()
@@ -47,6 +46,12 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(len(data['categories']))
+
+    # Test GET a category
+    def test_get_category_404(self):
+        res = self.client().get('categories/1')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
 
     # Test DELETE categories (Method not allowed)
     def test_delete_categories(self):
@@ -67,6 +72,30 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
+    # Test POST Questions with empty answer
+    def test_post_questions_empty_answer(self):
+        question = Question(
+            question='test',
+            answer=None,
+            category=1, difficulty=1)
+        payload = question.format()
+        res = self.client().post('/questions',  json=payload)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 500)
+        self.assertEqual(data['success'], False)
+
+    # Test POST Questions with string difficulty
+    def test_post_questions_string_difficulty(self):
+        question = Question(
+            question='test',
+            answer=None,
+            category=1, difficulty='one')
+        payload = question.format()
+        res = self.client().post('/questions',  json=payload)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 500)
+        self.assertEqual(data['success'], False)
+
     # Test GET questionss
     def test_get_questions(self):
         res = self.client().get('/questions')
@@ -83,6 +112,14 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
+    # Test GET questions of a categories that does not exists
+    def test_get_questions_of_categories_404(self):
+        category = 0
+        res = self.client().get('/categories/'+str(category)+'/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+
     # Test Delete a question
     def test_delete_question(self):
         question = Question.query.first()
@@ -90,6 +127,13 @@ class TriviaTestCase(unittest.TestCase):
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
+
+    # Test Delete a question wrong id
+    def test_delete_question_wrong_id(self):
+        res = self.client().delete('/questions/0')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 500)
+        self.assertEqual(data['success'], False)
 
     # Test search a question
     def test_search_question_random_string(self):
@@ -113,6 +157,16 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertTrue(len(data['questions']))
 
+    # Test search a question with int
+    def test_search_question_random_string(self):
+        payload = {
+            "searchTerm": 1223
+        }
+        res = self.client().post('/questions/search', json=payload)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 500)
+        self.assertEqual(data['success'], False)
+
     # Test quizzes
     def test_quizze(self):
         category = Category.query.first().format()
@@ -125,6 +179,20 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(len(data['question']))
+
+    # Test quizzes
+    def test_quizze_when_category_does_not_exists(self):
+        category = {
+            'type': 'someNoneExistingQuery'
+        }
+        payload = {
+            'previous_questions': [],
+            'quiz_category': category
+        }
+        res = self.client().post('/quizzes',  json=payload)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['forceEnd'], True)
 
     """
     TODO
