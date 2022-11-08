7eb08878a69555d25526cef784fb8fba155bb496
Hamid Khodabandehloo <hamid@dropbox.com>
2022-11-07 20:13:20 -0800

    fix py code style

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,8 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgresql://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}/{}".format(
+            'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,99 +25,102 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
-    
 
-    #Test POST Category
+    # Test POST Category
+
     def test_post_category(self):
         category = Category(type='test')
         payload = category.format()
-        res = self.client().post('/categories',  json = payload)
+        res = self.client().post('/categories',  json=payload)
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        
-    #Test GET categories
+
+    # Test GET categories
     def test_get_categories(self):
         res = self.client().get('/categories')
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(len(data['categories']))
-    
-    #Test DELETE categories (Method not allowed)
+
+    # Test DELETE categories (Method not allowed)
     def test_delete_categories(self):
         res = self.client().delete('/categories')
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 405)
         self.assertEqual(data['success'], False)
-    
-    #Test POST Questions
+
+    # Test POST Questions
     def test_post_questions(self):
-        question = Question(question = 'test', answer = 'test answer', category = 1, difficulty = 1)
+        question = Question(
+            question='test',
+            answer='test answer',
+            category=1, difficulty=1)
         payload = question.format()
-        res = self.client().post('/questions',  json = payload)
+        res = self.client().post('/questions',  json=payload)
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        
-    #Test GET questionss
+
+    # Test GET questionss
     def test_get_questions(self):
         res = self.client().get('/questions')
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(len(data['questions']))
-    
-    #Test GET questions of a categories
+
+    # Test GET questions of a categories
     def test_get_questions_of_categories(self):
         category = Category.query.first()
         res = self.client().get('/categories/'+str(category.id)+'/questions')
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        
-    #Test Delete a question
+
+    # Test Delete a question
     def test_delete_question(self):
         question = Question.query.first()
         res = self.client().delete('/questions/'+str(question.id))
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-    
-    #Test search a question
+
+    # Test search a question
     def test_search_question_random_string(self):
         payload = {
-            "searchTerm":"ham"
+            "searchTerm": "ham"
         }
-        res = self.client().post('/questions/search',  json = payload)
+        res = self.client().post('/questions/search',  json=payload)
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        
-    #Test search a question existing
+
+    # Test search a question existing
     def test_search_question_existing_question(self):
         question = Question.query.first()
         payload = {
-            "searchTerm":question.question
+            "searchTerm": question.question
         }
-        res = self.client().post('/questions/search',  json = payload)
+        res = self.client().post('/questions/search',  json=payload)
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(len(data['questions']))
 
-    #Test quizzes
+    # Test quizzes
     def test_quizze(self):
         category = Category.query.first().format()
         payload = {
-            'previous_questions':[],
-            'quiz_category':category
+            'previous_questions': [],
+            'quiz_category': category
         }
-        res = self.client().post('/quizzes',  json = payload)
+        res = self.client().post('/quizzes',  json=payload)
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
@@ -124,7 +128,8 @@ class TriviaTestCase(unittest.TestCase):
 
     """
     TODO
-    Write at least one test for each test for successful operation and for expected errors.
+    Write at least one test for each test
+    for successful operation and for expected errors.
     """
 
 
